20066916a0f680dc2a58e3fc0d9a8886cac718fb
srakurty <srakurty@192.168.179.1>
2017-08-24 15:41:41 +0530

    Hello

---
+++ pavan/src/co/pavan/Devops_Pavan.java
@@ -0,0 +1,13 @@
+package co.pavan;
+
+import java.util.Scanner;
+
+public class Devops_Pavan {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int a = sc.nextInt();
+		int b = a*a;
+		System.out.println(b);
+	}
+}
